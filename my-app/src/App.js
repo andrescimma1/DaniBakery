7951e8f7e1e27913236fa959fdcc8488c28c9e3b
DESKTOP-FIHAS8K\User <andrescimma1@gmail.com>
2021-05-19 22:15:01 -0300

    React implemented

---
+++ my-app/src/App.js
@@ -0,0 +1,136 @@
+import logo from './logo.svg';
+import './App.css';
+import './css/fonts.css';
+import alfajores from './img/alfajores.jpeg'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+function App() {
+  return (
+    
+    <div className="App">
+      <h1>Dulce Bocado</h1>
+      <header className="App-header">
+        <section class="header">
+
+        <div class="card-columns">
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={alfajores} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Alfajores de coco rellenos de dulce de leche.
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={alfajores} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Alfajores de coco rellenos de dulce de leche.
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={alfajores} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Alfajores de coco rellenos de dulce de leche.
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={alfajores} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Alfajores de coco rellenos de dulce de leche.
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={alfajores} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Alfajores de coco rellenos de dulce de leche.
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={alfajores} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Alfajores de coco rellenos de dulce de leche.
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={alfajores} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Alfajores de coco rellenos de dulce de leche.
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={alfajores} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Alfajores de coco rellenos de dulce de leche.
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={alfajores} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Alfajores de coco rellenos de dulce de leche.
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={alfajores} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Alfajores de coco rellenos de dulce de leche.
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={alfajores} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Alfajores de coco rellenos de dulce de leche.
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        </section>
+        
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
